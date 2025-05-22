889e6a1ba9ddd693aa1e67d131cc8639ba87b30d
anisss61 <noviantianis180@gmail.com>
2025-05-22 15:16:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,11 @@ st.write(
     "ini punya anis."
 )
 st.image("d5bd46347ff8022d4ab51895216282aa.jpg", width=200)
+
+st.header("Aplikasi Mengecek Nilai Genap/Ganjil")
+angka = st.number_input("Tulis sebuah angka:", value=0, step=1)
+
+if (angka % 2) == 0:
+    st.write(f"{angka} adalah Bilangan Genap")
+else:
+    st. write(f"{angka} adalah Bilangan Ganjil")
